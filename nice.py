50c991d058a17fbc47cca1be01939e24653f2d0b
kabdesh <37211501+kabdesh@users.noreply.github.com>
2018-03-09 16:47:09 +0600

    Создаем первый код

---
+++ nice.py
@@ -0,0 +1,25 @@
+number = int (input ("Введите число 1-12: "))
+if number == 1:
+    print("январь")
+elif number == 2:
+    print("Февраль")
+elif number == 3:
+    print("Февраль")
+elif number == 4:
+    print("Февраль")
+elif number == 5:
+    print("Февраль")
+elif number == 6:
+    print("Февраль")
+elif number == 7:
+    print("Февраль")
+elif number == 8:
+    print("Февраль")
+elif number == 9:
+    print("Февраль")
+elif number == 10:
+    print("Февраль")
+elif number == 11:
+    print("Февраль")
+elif number == 12:
+    print("Февраль")
